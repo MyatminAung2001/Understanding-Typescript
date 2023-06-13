4d6e7e519aec8d6016154564497eede56a16b8f6
MyatMinAung <myatminaung.dev@gmail.com>
2023-06-13 22:43:21 +0630

    Working with Numbers, Strings & Booleans

--- app.ts
+++ app.ts
@@ -1,9 +1,19 @@
-function add(number1: number, number2: number) {
-  return number1 + number2;
+function add(
+    number1: number,
+    number2: number,
+    printResult: boolean,
+    result: string
+) {
+    if (printResult) {
+        console.log(result + number1 + number2);
+    } else {
+        return number1 + number2;
+    }
 }
 
 const number1 = 5;
 const number2 = 5.5;
+const printResult = true;
+const resultPhrase = "Result is: ";
 
-const result = add(number1, number2);
-console.log(result);
+add(number1, number2, printResult, resultPhrase);
