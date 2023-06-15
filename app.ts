00fce951e938bb7ebdf638e62ecaea2c25b00268
MyatMinAung <myatminaung.dev@gmail.com>
2023-06-15 22:00:16 +0630

    Literal Types

--- app.ts
+++ app.ts
@@ -1,15 +1,30 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+    input1: number | string,
+    input2: number | string,
+    resultType: "as-number" | "as-string"
+) {
     let result;
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2;
+    if (
+        (typeof input1 === "number" && typeof input2 === "number") ||
+        resultType === "as-number"
+    ) {
+        result = +input1 + +input2;
     } else {
         result = input1.toString() + input2.toString();
     }
     return result;
+    // if (resultType === "as-number") {
+    //     return +result;
+    // } else {
+    //     return result.toString();
+    // }
 }
 
-const combinedAge = combine(10, 11);
+const combinedAge = combine(10, 11, "as-number");
 console.log(combinedAge);
 
-const combinedName = combine("Myatmin", " Aung");
+const combinedStringAge = combine("10", "11", "as-number");
+console.log(combinedStringAge);
+
+const combinedName = combine("Myatmin", " Aung", "as-string");
 console.log(combinedName);
