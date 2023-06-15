fa178464ef47d2cb622962021d91ab2fe23732ff
MyatMinAung <myatminaung.dev@gmail.com>
2023-06-15 20:50:09 +0630

    Nested Objects

--- app.ts
+++ app.ts
@@ -6,4 +6,24 @@ const person: {
     age: 21,
 };
 
-console.log(person.nickname);
+console.log(person);
+
+const nestedObjects: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    from: {
+        country: string;
+        city: string;
+    };
+} = {
+    name: "Myat Min Aung",
+    age: 21,
+    hobbies: ["swimming, football"],
+    from: {
+        country: "Myanmar",
+        city: "Yangon",
+    },
+};
+
+console.log(nestedObjects);
