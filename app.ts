fc39ef51c4d4786d02836108e2d554dc80f0b18b
MyatMinAung <myatminaung.dev@gmail.com>
2023-06-25 18:37:20 +0630

    type guard

--- app.ts
+++ app.ts
@@ -16,9 +16,53 @@ const e1: ElevatedEmployee = {
     startDate: new Date(),
 };
 
-console.log("e1", e1);
-
 type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+// type guards
+type UnknownEmployee = Admin | Employee;
+
+const printEmployeeInfo = (emp: UnknownEmployee) => {
+    console.log("Name: " + emp.name);
+    if ("privileges" in emp) {
+        console.log("Privileges: " + emp.privileges);
+    }
+    if ("startDate" in emp) {
+        console.log("StartDate: " + emp.startDate);
+    }
+};
+
+printEmployeeInfo(e1);
+
+class Car {
+    drive() {
+        console.log("Driving...");
+    }
+}
+
+class Truck {
+    drive() {
+        console.log("Driving a truck...");
+    }
+
+    loadCargo(amount: number) {
+        console.log("Loading cargo: " + amount);
+    }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+const useVehicle = (vehicle: Vehicle) => {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+};
+
+useVehicle(v1);
+useVehicle(v2);
